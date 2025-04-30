774fe174fdf793b85f886b89a496465d75ee57d5
Bn4crt <158043478+Bn4crt@users.noreply.github.com>
2025-04-30 08:22:33 +0100

    Update: logger.py

--- logger.py
+++ logger.py
@@ -1,13 +1,13 @@
 import boto3
 import json
 import os
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 def log_to_s3(payload, prefix="logs"):
     s3 = boto3.client("s3")
     log_bucket = os.environ.get("LOG_BUCKET", "windy-artifact-bucket")
-    timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H-%M-%S")
+    timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H-%M-%S")
     key = f"{prefix}/{timestamp}.json"
     s3.put_object(
         Bucket=log_bucket,
