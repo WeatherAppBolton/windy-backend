5c1ecca8158751347c676d7e5cb299dce726f219
Bn4crt <158043478+Bn4crt@users.noreply.github.com>
2025-05-03 18:38:56 +0100

    Delete: tests/test_registeruser.py

--- tests/test_registeruser.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from lambda_functions.RegisterUser.register_user import lambda_handler
-
-class TestRegisterUserHandler(unittest.TestCase):
-    def test_missing_fields(self):
-        event = { "body": '{"email": "test@example.com"}' }
-        result = lambda_handler(event, None)
-        assert result["statusCode"] == 400
